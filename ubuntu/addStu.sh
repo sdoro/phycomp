fcf2cbf51d56c1e10627e067b89ee7fbe6c9e3bc
sdoro <sandro.doro@gmail.com>
2023-01-17 17:40:33 +0100

    Fix little bugs

--- ubuntu/addStu.sh
+++ ubuntu/addStu.sh
@@ -2,3 +2,4 @@
 # deluser --remove-all-files studentestem
 adduser --gecos "Studente STEM" --disabled-password --home /home/studentestem studentestem
 echo -e "XXXXXXXX\nXXXXXXXX" | passwd studentestem
+adduser studentestem dialout
