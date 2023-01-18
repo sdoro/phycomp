920ff1d68ba84c75b4cd0ce1aab89bce82106bb8
sdoro <sandro.doro@gmail.com>
2023-01-18 08:12:04 +0100

    Cleanup

--- ubuntu/addStu.sh
+++ ubuntu/addStu.sh
@@ -1,5 +1,7 @@
-# adduser (before deluser to erase)
+# add user studentestem
+
 # deluser --remove-all-files studentestem
 adduser --gecos "Studente STEM" --disabled-password --home /home/studentestem studentestem
 echo -e "XXXXXXXX\nXXXXXXXX" | passwd studentestem
 adduser studentestem dialout
+# bot
