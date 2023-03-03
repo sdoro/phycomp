ee92222772e2a236c908685f5216adad8735a425
sdoro <sandro.doro@gmail.com>
2023-03-03 15:43:04 +0100

    Cosmetics

--- microbit/grove/lock/main.py
+++ microbit/grove/lock/main.py
@@ -3,11 +3,13 @@ from tm1637 import TM1637
 from LuchettoRotante import *
 import music
 
+# init display driver
+tm = TM1637(clk=pin2, dio=pin16)
+
+# init lock
 combinazione = [10, 20, 30]
 lucchetto = LucchettoRotante(combinazione)
 print(lucchetto)
-
-tm = TM1637(clk=pin2, dio=pin16)
 
 
 num = 0
@@ -18,7 +20,9 @@ display.show(im)
 dx = True
 
 while True:
+
     if button_a.was_pressed():
+        # accetta il valore sul display come mossa sul lucchetto
         print('a pressed')
         if dx:
             print('dx')
@@ -34,35 +38,42 @@ while True:
             im = Image.ARROW_E
             display.show(im)
             dx = True
+
     elif button_b.was_pressed():
+        # dai il comando apri al lucchetto
         print('b pressed')
         # prova ad aprire
         if lucchetto.apri():
             print("lucchetto aperto")
+            display.show(Image.HAPPY)
             music.play(music.PUNCHLINE)
             
         else:
             print("lucchetto NON aperto")
+            display.show(Image.SAD)
             music.play(music.FUNERAL)
 
         break
         
     if pin_logo.is_touched():
-        # reset
+        # dai il comando reset al lucchetto
         print("reset")
         lucchetto.reset()
         lock = 0
         im = Image.ARROW_E
         display.show(im)
         dx = True
-        
+
     sleep(1000)
+    # transform a value from Potentiometer [0, 1021] into a number between 0 and 39
     val = pin0.read_analog()
     tmp = val*39//1021
     if tmp != lock:
+        # if rotated make a "click"
         lock = tmp
         music.pitch(800, 6)
-    #display.scroll(str(lock))
+
     tm.number(lock)
-    #music.pitch(800, 6)
-    print('val: ', val, ' lock: ', lock)
+    print('val[0,1021]: ', val, ' lock[0,39]: ', lock)
+
+# bot
