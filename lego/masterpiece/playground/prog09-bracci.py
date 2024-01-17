cb36269eca5c4b7eb92d4fb23b24b7925b961888
sdoro <sandro.doro@gmail.com>
2024-01-17 20:12:30 +0100

    Add buttom for move arms

--- lego/masterpiece/playground/prog09-bracci.py
+++ lego/masterpiece/playground/prog09-bracci.py
@@ -1,6 +1,6 @@
 
 import motor
-from hub import port
+from hub import port, button
 import sys
 
 import runloop
@@ -20,18 +20,24 @@ async def head(n, direction):
     else:
         await motor.run_for_degrees(port.D, n, -200)
 
-VALUE = 40
+VALUE = 20
 async def main():
-    
-    #await head(VALUE, UP)                                # ONE AT TIME
-    #await tail(VALUE, DOWN)                              # ONE AT TIME
-    #await head(VALUE, DOWN)                              # ONE AT TIME
-    #await tail(VALUE, UP)                                # ONE AT TIME
 
-    runloop.run(*[head(VALUE, UP),tail(VALUE, DOWN)])     # SIMULTANEUS
-    runloop.run(*[head(VALUE, DOWN),tail(VALUE, UP)])     # SIMULTANEUS
+    cnt = 0
+    while True:
+        if button.pressed(button.LEFT):
+            await tail(VALUE, DOWN)
+            await head(VALUE, DOWN)
+            cnt += 1
+        if button.pressed(button.RIGHT):
+            await head(VALUE, UP)
+            await tail(VALUE, UP)
+            cnt += 1
+
+        if cnt > 10:              # numero max di attivazioni bottoni SX e DX
+            break
 
     print('fine main')
-    sys.exit(0)               
+    sys.exit(0)
 
 runloop.run(*[main()])
