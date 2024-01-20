db92f4037bc26a2d0921a65f79e59c0de40bbc43
sdoro <sandro.doro@gmail.com>
2024-01-20 16:08:25 +0100

    Fixed

--- lego/masterpiece/playground/prog14-lineFollower-01.py
+++ lego/masterpiece/playground/prog14-lineFollower-01.py
@@ -2,23 +2,23 @@ from hub import port
 import motor_pair, color_sensor, runloop
 
 # Constants for Drive Base 1
-motor_pair.pair(motor_pair.PAIR_1, port.C, port.D)
+motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
 
 # Follow the right side of black line (Black-White edge). NOTE: Our test was run on a black-white mat.
 # If your mat has many colors, you will have to lower the threshold to avoid other colors.
 # To follow a White-Black edge, change the IF condition from < 50 to > 50
 # To use color mode, import color, and use condition:
 # if (color_sensor.color(port.A) == color.BLACK)
-async def line_follow_forever():
+async def line_follow_forever(v):
     while (True):
-        if (color_sensor.reflection(port.A) < 50): # sensor is on Black. Lower threshold as needed for your case.
+        if (color_sensor.reflection(port.B) < 50): # sensor is on Black. Lower threshold as needed for your case.
             # Turn right, i.e. away from Black
-            motor_pair.move(motor_pair.PAIR_1, 30, velocity = 300)
+            motor_pair.move(motor_pair.PAIR_1, 50, velocity = v)
         else: # sensor is on white
             # Turn left, i.e. towards Black
-            motor_pair.move(motor_pair.PAIR_1, -30, velocity = 300)
+            motor_pair.move(motor_pair.PAIR_1, -50, velocity = v)
 
 async def main():
-    await line_follow_forever()
+    await line_follow_forever(200)
 
 runloop.run(main())
