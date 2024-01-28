2ac1a5a512ddcebe36e86f69a79b694cfd78e2a6
sdoro <sandro.doro@gmail.com>
2024-01-28 10:37:40 +0100

    Change wheel for ADB

--- lego/masterpiece/playground/distance.py
+++ lego/masterpiece/playground/distance.py
@@ -1,5 +1,5 @@
-WHEEL_CIRCUMFERENCE_DB1 = 17.5  # cm, this is a constant for your robot
-WHEEL_CIRCUMFERENCE_ADB = 27.63 # cm, this is a constant for your robot
+WHEEL_CIRCUMFERENCE_DB1 = 17.5  # cm, this is a constant for DB1
+WHEEL_CIRCUMFERENCE_ADB = 27.63 # cm, this is a constant for ADB
 
 def degreesForDistance(distance_cm): # input must be in the same unit as WHEEL_CIRCUMFERENCE
     return int((distance_cm/WHEEL_CIRCUMFERENCE_ADB) * 360) # Add multiplier for gear ratio if needed
