43c80b615504c180ebc2c3b6a72c58880ab207a4
sdoro <sandro.doro@gmail.com>
2024-01-28 10:13:29 +0100

    Add mor speed, stop to road intersection and print path length

--- lego/masterpiece/playground/prog14-lineFollower-02.py
+++ lego/masterpiece/playground/prog14-lineFollower-02.py
@@ -4,19 +4,29 @@ import motor, motor_pair, color_sensor, runloop, sys
 # Constants for Drive Base 1
 motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
 
+# rest position for motor moves clockwise
+motor.reset_relative_position(port.E, 0)
+
 # Follow the right side of black line (Black-White edge).
 # To follow a White-Black edge, change the error condition to (reflection - 50)
 async def line_follow_forever(v):
-    while (True):
+    while True:
+        if color_sensor.reflection(port.F) < 30:              # sensore DX indica incrocio?
+            break
         # Compute the error
-        error = 50 - color_sensor.reflection(port.B)
+        error = 50 - color_sensor.reflection(port.B)          # il sensore SX segue linea
         # Compute the correction by multiplying the error
         # by a Constant of Proportionality
-        myConstant = 4
-        correction = int(error * 0.5) * myConstant
+        myConstant = 2.5
+        correction = int(error * 0.5 * myConstant)
         motor_pair.move(motor_pair.PAIR_1, correction, velocity = v)
 
+    motor_pair.stop(motor_pair.PAIR_1)
+    print(motor.relative_position(port.E))                     # print path lenght
+
 async def main():
-    await line_follow_forever(100)
+    await line_follow_forever(250)
+    sys.exit(0)
 
 runloop.run(main())
+
