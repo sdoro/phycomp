3481cc9b6b63b5a2f51ddc502db49631a07c4da0
sdoro <sandro.doro@gmail.com>
2024-01-31 11:54:00 +0100

    Fix record data

--- lego/masterpiece/playground/prog16-lineFollower-02-discovery.py
+++ lego/masterpiece/playground/prog16-lineFollower-02-discovery.py
@@ -6,30 +6,6 @@ import motor, motor_pair, color_sensor, runloop, sys
 
 import time
 
-# reset position for right motor that moves clockwise
-motor.reset_relative_position(port.E, 0)
-
-halt = False
-
-async def contaCentimetri():
-    global path
-    global halt
-    global start
-
-    start = time.ticks_ms()
-    
-    path = []
-    halt = False
-
-    while not halt:
-        rp = motor.relative_position(port.E)
-        e = [rp, time.ticks_ms() - start]
-        path.append(e)
-        #print('path tmp: ', path)
-        await runloop.sleep_ms(200)                    # sleep only this coroutine
-
-    print('contaCentimetri terminata')
-
 # The drive motors are in port A (left) and port E (right).
 motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
 
@@ -42,7 +18,17 @@ async def line_follow_forever(v):
     global path
     global halt
 
+    # reset position for right motor that moves clockwise
+    motor.reset_relative_position(port.E, 0)
+
+    start = time.ticks_ms()
+    path = []
+    steer = []
+    lastCorrection = 0
+    cnt = -1
+
     while True:
+        cnt += 1
         if color_sensor.reflection(port.F) < 30:        # sensore DX indica incrocio?
             break                                        # fermati!
         # Compute the error: please attention to inversion!
@@ -53,20 +39,28 @@ async def line_follow_forever(v):
         # by a Constant of Proportionality
         myConstant = 4.5
         correction = int(error * 0.5 * myConstant)
+        if cnt%1000 == 0:
+            #print(lastCorrection, correction)
+            rp = motor.relative_position(port.E)
+            t = time.ticks_ms() - start
+            e = [rp, t]
+            path.append(e)
+            steer.append([correction, t])
+        lastCorrection = correction
         motor_pair.move(motor_pair.PAIR_1, correction, velocity = v)
 
     motor_pair.stop(motor_pair.PAIR_1)
 
-    halt = True
-    await runloop.sleep_ms(2000)                        # sleep to wait other coroutine finished
     lt = motor.relative_position(port.E) / 360 * WHEEL_CIRCUMFERENCE
     print(round(motor.relative_position(port.E),2), 'gradi')                            # print lenght track
-    print(round(lt, 2), 'cm')                               # print lenght track
-    print('path completo: ', path)
+    print(round(lt, 2), 'cm')                            # print lenght track
+    print('path percorso: ', path)
+    print('steering: ', steer)
 
 async def main():
     await line_follow_forever(125)
 
     sys.exit(0)
 
-runloop.run(main(), contaCentimetri())
+runloop.run(main())
+
