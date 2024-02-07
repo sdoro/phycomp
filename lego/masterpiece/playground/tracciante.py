92df91939c46f2ec73d3ef53f75ca2c273d57076
sdoro <sandro.doro@gmail.com>
2024-02-07 17:18:29 +0100

    Partial fix

--- lego/masterpiece/playground/tracciante.py
+++ lego/masterpiece/playground/tracciante.py
@@ -1,7 +1,7 @@
 
 # test su 3 tiles (Terminatore, Curva, Terminatore): T+C+T
 
-from hub import port, button
+from hub import port, button, sound
 import motor, motor_pair, color_sensor, runloop, sys
 
 import time
@@ -16,11 +16,29 @@ WHEEL_CIRCUMFERENCE = 27.63
 def left_pressed():
     return button.pressed(button.LEFT) > 0
 
+# Questa coroutine controlla continuamente se il pulsante sinistro è premuto.
+async def check_button():
+    global leftPressed
+    sound.beep(880, 200, 100)
+
+    leftPressed = False
+    while True:
+        # Attendi fino a quando il pulsante sinistro viene premuto,
+        while not left_pressed():
+            await runloop.sleep_ms(1)
+        # Quando lo premi emetti un breve segnale acustico.
+        leftPressed = True
+        sound.beep(880, 200, 100)
+        # Attendi fino al rilascio del pulsante sinistro.
+        while left_pressed():
+            await runloop.sleep_ms(1)
+        leftPressed = False
+
+leftPressed = False
+
 # Follow the right side of black line (Black-White edge).
 # To follow a White-Black edge, change the error condition to (reflection - 50)
 async def line_follow_forever(v):
-    global path
-    global halt
 
     # reset position for right motor that moves clockwise
     motor.reset_relative_position(port.E, 0)
@@ -28,29 +46,35 @@ async def line_follow_forever(v):
     start = time.ticks_ms()
     path = [[0, 0]]
 
+    global leftPressed
+
     while True:
         rp = motor.relative_position(port.E)
-        cond = (rp < 1250) or (rp > 1450)
+        #cond = (rp < 1250) or (rp > 1450)
         if color_sensor.reflection(port.F) < 30:
             break
-        if color_sensor.reflection(port.F) < 30 and cond:        # sensore DX indica incrocio?
-            break
-            rp = motor.relative_position(port.E)
-            break
-            if (rp < 1250) or (rp > 1450):
-                motor_pair.stop(motor_pair.PAIR_1)
-                print('trovato arresto: ', rp)
-                break                                        # fermati!
+        #if color_sensor.reflection(port.F) < 30 and cond:        # sensore DX indica incrocio?
+        #    break
+        #    rp = motor.relative_position(port.E)
+        #    break
+        #    if (rp < 1250) or (rp > 1450):
+        #        motor_pair.stop(motor_pair.PAIR_1)
+        #        print('trovato arresto: ', rp)
+        #        break                                        # fermati!
         # Compute the error: please attention to inversion!
-        #error = 50 - color_sensor.reflection(port.B)    # il sensore SX (B) segue linea a SX ha BLACK
-        error = color_sensor.reflection(port.B) - 50    # il sensore SX (B) segue linea a DX ha BLACK
+        #error = 50 - color_sensor.reflection(port.B)       # il sensore SX (B) segue il lato DX della linea nera
+        error = color_sensor.reflection(port.B) - 50        # il sensore SX (B) segue il lato SX della linea nera
 
         # Compute the correction by multiplying the error
-        # by a Constant of Proportionality
-        myConstant = 4.5
-        correction = int(error * 0.5 * myConstant)
+        # by a Constant of Proportionality (p)
+        p = 2.25
+        # Since the maximum absolute value of error is 50, the correction ranges from -112 to 112
+        correction = int(error * p)
+        # clamp the correction from -100 to 100 because SP3 doesn’t seem to do it internally.
+        correction = min(100, max(-100, correction))
         motor_pair.move(motor_pair.PAIR_1, correction, velocity = v)
-        if left_pressed():
+        if leftPressed:
+            print('pigiato left')
             rp = motor.relative_position(port.E)
             t = time.ticks_ms() - start
             if (rp - path[-1][0]) > 25:    # troppo vicini? non registro
@@ -65,8 +89,8 @@ async def line_follow_forever(v):
     print('path percorso: ', path)
 
 async def main():
+    leftPressed = False
     await line_follow_forever(125)
+    sys.exit(0)
 
-    #sys.exit(0)
-
-runloop.run(main())
+runloop.run(check_button(),main())
