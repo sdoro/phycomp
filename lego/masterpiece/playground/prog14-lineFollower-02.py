c6877b48767bcb942399ed207849127d0d8aaa39
sdoro <sandro.doro@gmail.com>
2024-02-07 15:20:25 +0100

    Updated and fixed

--- lego/masterpiece/playground/prog14-lineFollower-02.py
+++ lego/masterpiece/playground/prog14-lineFollower-02.py
@@ -25,27 +25,35 @@ async def line_follow_forever(v):
         if color_sensor.reflection(port.F) < 30:            # sensore DX indica incrocio?
             break                                           # fermati!
         # Compute the error: please attention to inversion!
-        #error = 50 - color_sensor.reflection(port.B)       # il sensore SX (B) segue linea a SX ha BLACK
-        error = color_sensor.reflection(port.B) - 50        # il sensore SX (B) segue linea a DX ha BLACK
+        #error = 50 - color_sensor.reflection(port.B)       # il sensore SX (B) segue il lato DX della linea nera
+        error = color_sensor.reflection(port.B) - 50        # il sensore SX (B) segue il lato SX della linea nera
 
         # Compute the correction by multiplying the error
-        # by a Constant of Proportionality
-        myConstant = 4.5
-        correction = int(error * 0.5 * myConstant)
+        # by a Constant of Proportionality (p)
+        p = 2.25
+        # Since the maximum absolute value of error is 50, the correction ranges from -112 to 112
+        correction = int(error * p)
+        # clamp the correction from -100 to 100 because SP3 doesn’t seem to do it internally.
+        correction = min(100, max(-100, correction))
         motor_pair.move(motor_pair.PAIR_1, correction, velocity = v)
 
-        if cnt%250 == 0:                                    # save a sample avery 250 loop step
+        if cnt % 250 == 0:                                  # save a sample avery 250 step
             path.append(motor.relative_position(port.E))
 
     motor_pair.stop(motor_pair.PAIR_1)
     rp = motor.relative_position(port.E)
     print(round(rp, 2), 'gradi')                            # print lenght track
+    print(round(rp/360, 2), 'rotazioni')                    # print rotations
     lt = rp / 360 * WHEEL_CIRCUMFERENCE
-    print(round(lt, 2), 'cm')                               # print lenght track
+    print(round(lt, 2), 'cm')                               # print lenght track in cm.
     print(path)
 
 async def main():
-    await line_follow_forever(125)
+    await line_follow_forever(150)
     sys.exit(0)
+
+'''
+Sembra proprio che il coef. sia 2.25 e la velocità sui 150 (175 max)
+'''
 
 runloop.run(main())
