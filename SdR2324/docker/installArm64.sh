a0d0c457d8dfc4e7007ee21ce99b09351b77b0f9
sdoro <sandro.doro@gmail.com>
2024-04-13 06:10:46 +0200

    Add yes to install

--- SdR2324/docker/installArm64.sh
+++ SdR2324/docker/installArm64.sh
@@ -13,4 +13,4 @@ echo \
 sudo apt update
 
 # To install the latest version
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
