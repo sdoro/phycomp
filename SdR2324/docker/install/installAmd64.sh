b00818f1fb016e63077bbbe420e0a004625d1cd5
sdoro <sandro.doro@gmail.com>
2024-04-18 13:23:56 +0200

    Add docker install for amd64

---
+++ SdR2324/docker/install/installAmd64.sh
@@ -0,0 +1,10 @@
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-compose-plugin
+
+# Install docker-compose
+sudo ln -s /usr/libexec/docker/cli-plugins/docker-compose /usr/bin/docker-compose
