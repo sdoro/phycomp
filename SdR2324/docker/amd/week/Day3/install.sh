0e01f861dbd3ac8db8ec9ae2dd2d767fa43038cd
sdoro <sandro.doro@gmail.com>
2024-08-19 20:35:46 +0200

    Continuing day 4 ...

--- SdR2324/docker/amd/week/Day3/install.sh
+++ SdR2324/docker/amd/week/Day3/install.sh
@@ -1,8 +1,8 @@
 cd ~
-tar zxf ros2-learning-week-day-3-basic--2024-08-14--bd651c1ff31b.zip
+tar /mnt/share/ros2-learning-week-day-3-basic--2024-08-14--bd651c1ff31b.tgz
 apt update
 apt install -y vim mc less
-apt install ros-jazzy-ros2-control
-apt install ros-jazzy-gz-ros2-control-demos
-apt install ros-jazzy-ros-gz ros-jazzy-ros-gz-sim
+apt install -y ros-jazzy-ros2-control
+apt install -y ros-jazzy-gz-ros2-control-demos
+apt install -y ros-jazzy-ros-gz ros-jazzy-ros-gz-sim
 # bot
---
+++ SdR2324/docker/amd/week/Day4/readme.txt
@@ -0,0 +1,11 @@
+
+# redfine HOME
+source /mnt/share/first.sh
+
+# install packages for day 4
+/mnt/share/course_install.sh
+
+# copy project
+tar zxf /mnt/share/ros2-learning-week-day-4-under--2024-08-03--e98949628832.tgz
+
+# bot
