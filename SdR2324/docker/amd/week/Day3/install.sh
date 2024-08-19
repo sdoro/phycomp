3aec11a9768aff61141af749839339c1bdc41dd0
sdoro <sandro.doro@gmail.com>
2024-08-19 12:56:05 +0200

    Add Day 3

--- SdR2324/docker/amd/week/Day3/install.sh
+++ SdR2324/docker/amd/week/Day3/install.sh
@@ -1,5 +1,5 @@
 cd ~
-unzip /mnt/share/ros2-learning-week-day-3-basic--2024-08-14--bd651c1ff31b.zip
+tar zxf ros2-learning-week-day-3-basic--2024-08-14--bd651c1ff31b.zip
 apt update
 apt install -y vim mc less
 apt install ros-jazzy-ros2-control
