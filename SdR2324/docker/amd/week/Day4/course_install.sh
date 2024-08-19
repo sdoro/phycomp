3122bcf22d104427a2b028a0cb8337740b46670c
sdoro <sandro.doro@gmail.com>
2024-08-19 20:18:15 +0200

    Starting day 4 ...

---
+++ SdR2324/docker/amd/week/Day4/course_install.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install ros-jazzy-robot-localization -y
+sudo apt install ros-jazzy-controller-manager -y
+sudo apt install ros-jazzy-ros2-control -y
+sudo apt install ros-jazzy-ros2-controllers -y
+sudo apt install ros-jazzy-gz-ros2-control -y
---
+++ SdR2324/docker/amd/week/Day4/install.sh
@@ -0,0 +1,8 @@
+cd ~
+tar zxf ros2-learning-week-day-4-under--2024-08-03--e98949628832.tgz
+apt update
+apt install -y vim mc less
+apt install ros-jazzy-ros2-control
+apt install ros-jazzy-gz-ros2-control-demos
+apt install ros-jazzy-ros-gz ros-jazzy-ros-gz-sim
+# bot
