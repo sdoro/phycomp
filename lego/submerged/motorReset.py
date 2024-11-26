b1520d480b19a9fd987e68f2e22db29a9187bd6f
sdoro <sandro.doro@gmail.com>
2024-11-26 22:47:12 +0100

    Add docstring for stall and sound

--- lego/submerged/motorReset.py
+++ lego/submerged/motorReset.py
@@ -1,18 +1,28 @@
-from hub import light_matrix, port
+from hub import light_matrix, port, sound
 import motor
 
 import runloop
 import sys
 
 async def main():
-    motor.run_to_absolute_position(port.A, 20, 1110)
-    await runloop.sleep_ms(1000)   # wait 1 second to allow execute previous command
+    '''
+    Stall detection allowed the code to move on to the next line even when the motor got
+    stuck (unable to complete its move). The beep played even though the motor did not
+    finish completing its command.
+    https://primelessons.org/en/ProgrammingLessons/MovingObjectsStall.pdf
+    '''
 
-    motor.run_to_absolute_position(port.A, -20, 1110)
+    motor.run_to_absolute_position(port.A,  40, 1110)
     await runloop.sleep_ms(1000)   # wait 1 second to allow execute previous command
+    await sound.beep()
 
-    motor.run_to_absolute_position(port.A, 0, 1110)
+    motor.run_to_absolute_position(port.A, -40, 1110)
     await runloop.sleep_ms(1000)   # wait 1 second to allow execute previous command
+    await sound.beep()
+
+    motor.run_to_absolute_position(port.A,   0, 1110)
+    await runloop.sleep_ms(1000)   # wait 1 second to allow execute previous command
+    await sound.beep()
 
 runloop.run(main())
 
