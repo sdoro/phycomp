19f07f414abc11bcccbff5e974460b3953d4b6b3
sdoro <sandro.doro@gmail.com>
2025-01-19 10:13:59 +0100

    Changed amixer commands

--- lego/submerged/underwater/Alsa.sh
+++ lego/submerged/underwater/Alsa.sh
@@ -1,2 +1,8 @@
-amixer cset numid=3 98%
-amixer cset numid=1 98%,98%
+#amixer cset numid=3 98%
+amixer sset Master Playback Volume 98%
+#amixer sset Speaker Playback Volume 98%
+
+#amixer cset numid=1 95%
+amixer sset Capture Volume 95%
+#amixer sset Mic Capture Volume 95%
+
--- lego/submerged/underwater/alsa.sh
+++ lego/submerged/underwater/alsa.sh
@@ -1,6 +1,21 @@
+# amixer controls
 #numid=4,iface=MIXER,name='Master Playback Switch'
 #numid=3,iface=MIXER,name='Master Playback Volume'
 #numid=2,iface=MIXER,name='Capture Switch'
 #numid=1,iface=MIXER,name='Capture Volume'
-amixer cset numid=3 45%
-amixer cset numid=1 95%,95%
+
+#amixer scontrols
+#Simple mixer control 'Master',0
+#Simple mixer control 'Capture',0
+
+# -----------------------------------------------------
+
+# amixer cset numid=3 45%
+amixer sset Master,0 45%
+#amixer sset Speaker Playback Volume 45%
+# amixer cset numid=6 45%
+
+#amixer cset numid=1 95%
+amixer sset Capture,0 95%
+#amixer sset Mic Capture Volume 95%
+#amixer cset numid=8 95%
